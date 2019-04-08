218a5e8befd8a3f1219b9cc0f7b9a8f013c86a73
juancruzalric94 <juancruzalric94@outlook.es>
2019-04-08 12:12:00 -0300

    implenting animation and animationController for the cat

--- animation_cat/lib/src/screens/home.dart
+++ animation_cat/lib/src/screens/home.dart
@@ -7,18 +7,63 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
-class _HomeState extends State<Home> {
+class _HomeState extends State<Home> with TickerProviderStateMixin{
+
+  Animation<double>catAnimation;
+  AnimationController catController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+
+    catAnimation = Tween(begin: 0.0, end: 100.0)
+      .animate(
+        CurvedAnimation(
+          parent: catController,
+          curve: Curves.easeIn,
+        ),
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Animation!'),
       ),
-      body: buildAnimation(),
+      body: GestureDetector(
+        child: buildAnimation(),
+        onTap: onTap,
+      ),
+      
+      
     );
   }
 
+  void onTap(){
+    if (catController.status == AnimationStatus.completed){
+      catController.reverse();
+    } else if (catController.status == AnimationStatus.dismissed){
+      catController.forward();
+    }
+  }
+
   Widget buildAnimation() {
-      return Cat();
+      return AnimatedBuilder(
+        animation: catAnimation,
+        builder: (BuildContext context, Widget child){
+          return Container(
+            child: child,
+            margin: EdgeInsets.only(top: catAnimation.value),
+          );
+        },
+        child: Cat(),
+      );
   }
 }
